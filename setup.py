949e5b8cd88be5e36009cb5f8e67e8c85b2555f4
Kishan S <senthilkumarkishan@gmail.com>
2025-02-25 21:19:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='charsay',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     author='Kishan S',
     author_email='senthilkumarkishan@gmail.com',
