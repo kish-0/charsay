7d9e2a899e83cc56fca1c82133d42996813dc230
Kishan <senthilkumarkishan@gmail.com>
2025-03-17 16:34:25 +0530

    charsay v0.1.3

--- charsay/charsay.py
+++ charsay/charsay.py
@@ -1,90 +1,96 @@
+import random
 from .speechbubble import say
-
-def bart(inp):
-    s = say(inp)
-    print(rf"""   
-                 |          
-    |\/\/\/|     |
-    |      |     | 
-    |      |     |
-    | (o)(o)     |
-    C      _)    |
-    | ,___| ____/
-    |   /    
-   /____\    
-  /      \ """)
-    
-def homer(inp):
-    s = say(inp)
-    print(rf""" 
-                |
-     __&__      |
-    /     \     |
-    |       |   |  
-    |  (o)(o)   |
-    C   .---_)  | 
-    | |.___| __/    
-    |  \__/     
-   /_____\     
-  /_____/ \    
- /         \ """)
-
-def marge(inp):
-    s = say(inp)
-    print(rf"""
- |         
- |              (####)
- |            (#######)
- |          (#########)
- |         (#########)
- |        (#########)
- |       (#########)
- |      (#########)
- |     (#########)
- |    (#########) 
- |     (o)(o)(##)
-  \  ,_C     (##)
-   \ /___,   (##)    
-      \     (#) 
-       |    |   
-       OOOOOO  
-      /      \
-""")
-    
-def lisa(inp):
-    s = say(inp)
-    print(rf"""
- |          
- |   /\ /\  /\      
- |   | V  \/  \---. 
- |    \_        /   
- |     (o)(o)  <__. 
- |    _C         /  
-  \_ /____,   )  \  
-       \     /----' 
-        ooooo       
-       /     \
-    """)
-    
-def maggie(inp):
-    s = say(inp)
-    print(rf"""
-       /\
- .----/  \----.    ________________
-  \          /    0                0
-.--\ (o)(o) /__.  |*slurp* ga ga ga|
- \     ()     /   0________________0 
-  >   (C_)   < __/
- /___\____/___\
-    /|    |\
-   /        \ """)
-    
+from .content import Homer, Bart, Marge, Lisa, Maggie
+characters = ['homer', 'bart', 'marge', 'lisa', 'maggie']
+from .content import lucky_list
 
 
 
-def main():
-    s = "Hello, world!"
-    maggie(s)
+class Charactersay:
+    def __init__(self, horizontal_length=60):
+        self.maxlength = horizontal_length
+        self.string = "Hello, world!"
+        self.internalcall = False
+
+    def __str__(self):
+        return("Character say!")
     
-if __name__ == "__main__":
-    main()
+    
+    def imfeelinglucky(self):                                # Generates a random character saying a random lucky-line from the content file
+        x = random.choice(characters)
+        self.internalcall = True                             # To indicate that the character funcs are being called internally
+        say(random.choice(lucky_list), self.maxlength)
+        match x:
+            case 'homer':
+                self.homer()
+            case 'marge':
+                self.marge()
+            case 'bart' :
+                self.bart()
+            case 'lisa' :
+                self.lisa()
+            case 'maggie':
+                self.maggie()
+                
+        self.internalcall = False
+
+    def bart(self):
+        if not self.internalcall:  
+            say(self.string, self.maxlength)
+        print(Bart)
+
+    def homer(self):
+        if not self.internalcall:                            # Should not say bcz, when its internally called, the random string musnt clash with given string
+            say(self.string, self.maxlength)
+        print(Homer)
+
+    def marge(self):
+        if not self.internalcall:  
+            say(self.string, self.maxlength)
+        print(Marge)
+        
+    def lisa(self):
+        if not self.internalcall:  
+            say(self.string, self.maxlength)
+        print(Lisa)
+        
+    def maggie(self):
+        if not self.internalcall:  
+            say(self.string, self.maxlength)
+        print(Maggie)
+
+
+    @property
+    def string(self):
+        return self._string
+    
+    @string.setter
+    def string(self, string):
+        string = str(string)
+        self._string = string
+
+
+    @property
+    def maxlength(self):
+        return self._maxlength
+    
+    @maxlength.setter
+    def maxlength(self, maxlength):
+        try:
+            maxlength = int(maxlength)
+        except ValueError:
+            raise ValueError("maxlength needs to be <class 'int'> or <class 'float'>")
+        
+        if maxlength < 15:
+            raise ValueError("'maxlength' cannot be lower than 15")
+        self._maxlength = maxlength
+    
+
+
+
+
+
+
+
+    
+
